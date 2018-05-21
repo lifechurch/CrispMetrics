0e75ed0df04ae310d0584ab7f45c463a961de7c0
Adam Freeman <shavenyaknz@gmail.com>
2018-05-21 23:11:11 +1200

    Rewrote not to use async

--- src/index.js
+++ src/index.js
@@ -4,35 +4,41 @@ const Crisp = require('node-crisp-api');
 let config;
 let CrispClient = new Crisp();
 
-async function main() {
-  try {
-    config = await (new Config()).getValues();
-  } catch (e) {
-    console.error('ERROR: Encountered trying to load the config:', e);
+function main() {
+  let cf = new Config();
+
+  cf.getValues().then( (config) => config )
+  .then( (config) => CrispClient.authenticate(config.auth.identifier, config.auth.key) )
+  .then( (cc) => CrispClient.userProfile.get())
+  .then( (profile) => CrispClient.userWebsites.get() )
+  .then( (websites) => {
+    console.log(`Can see ${websites.length} git diff
+    sites`);
+    let sites = websites.map(iterateSites);
+    return websites[0];
+  })
+  // NOTE: I think you need soe kind of admin rights to get operators
+  // .then( (site) => {
+  //   console.log('Site:', site.name);
+  //   return CrispClient.websiteOperators.getList(site.id);
+  // }).then( (peeps) => console.log('Peeps:', peeps ) )
+  .then( (site) => {
+    console.log('Name:', site.name);
+    return CrispClient.websiteConversations.getList(site.id, 0);
+  }).then ( (page1) => console.log('Convos:', page1) )
+  .then( () => {
+    // All done, finish up
+    console.log('Done !');
+    process.exit();
+  }).catch(function (e) {
+    console.error('ERROR: Encountered trying to do something:', e);
     process.exit(1);
-  }
+  });
+}
 
-  try {
-    await CrispClient.authenticate(config.auth.identifier, config.auth.key);
-
-    let myProfile = await CrispClient.userProfile.get();
-
-    console.log("Hello:", myProfile.first_name);
-
-    let websites = await CrispClient.userWebsites.get();
-    console.log('Websites:', websites);
-    
-    let site = websites[0];
-    let settings = await CrispClient.websiteSettings.get(site.id);
-    console.log(`Settings for ${site.name}: ${settings}`);
-
-    let converstaions = await CrispClient.websiteConversations.getList(site.id, 0);
-    console.log('Conversations:', converstaions);
-
-    console.log('Finished.');
-  } catch (e) {
-    console.error('ERROR: Encountered an Error while calling the API:', e);
-  }
+function iterateSites(site) {
+  console.log('Saw', site.name, 'with id:', site.id);
+  return site.id;
 }
 
 main();
