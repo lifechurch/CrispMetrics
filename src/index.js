24b527f189cf1976c700ad5e524c82461ac010e7
adamf <adam.freeman@sli-systems.com>
2018-05-23 13:44:34 +1200

    Putting back async

--- src/index.js
+++ src/index.js
@@ -4,41 +4,48 @@ const Crisp = require('node-crisp-api');
 let config;
 let CrispClient = new Crisp();
 
-function main() {
-  let cf = new Config();
+async function processSite(site) {
+  console.log('Saw', site.name, 'with id:', site.id);
 
-  cf.getValues().then( (config) => config )
-  .then( (config) => CrispClient.authenticate(config.auth.identifier, config.auth.key) )
-  .then( (cc) => CrispClient.userProfile.get())
-  .then( (profile) => CrispClient.userWebsites.get() )
-  .then( (websites) => {
-    console.log(`Can see ${websites.length} git diff
-    sites`);
-    let sites = websites.map(iterateSites);
-    return websites[0];
-  })
-  // NOTE: I think you need soe kind of admin rights to get operators
-  // .then( (site) => {
-  //   console.log('Site:', site.name);
-  //   return CrispClient.websiteOperators.getList(site.id);
-  // }).then( (peeps) => console.log('Peeps:', peeps ) )
-  .then( (site) => {
-    console.log('Name:', site.name);
-    return CrispClient.websiteConversations.getList(site.id, 0);
-  }).then ( (page1) => console.log('Convos:', page1) )
-  .then( () => {
-    // All done, finish up
-    console.log('Done !');
-    process.exit();
-  }).catch(function (e) {
-    console.error('ERROR: Encountered trying to do something:', e);
-    process.exit(1);
-  });
+  // CrispClient.websiteOperators.getList(site.id); - Need admin account to get this probably
+
+  let settings = await CrispClient.websiteSettings.get(site.id);
+  console.log(`Settings for ${site.name}: ${settings}`);
 }
 
-function iterateSites(site) {
-  console.log('Saw', site.name, 'with id:', site.id);
-  return site.id;
+async function processConversations(site) {
+  let conversations = await CrispClient.websiteConversations.getList(site.id, 0);
+  console.log('Conversations:', conversations);   
+}
+
+async function main() {
+  try {
+    config = await (new Config()).getValues();
+  } catch (e) {
+    console.error('ERROR: Encountered trying to load the config:', e);
+    process.exit(1);
+  }
+
+  try {
+    await CrispClient.authenticate(config.auth.identifier, config.auth.key);
+
+    let myProfile = await CrispClient.userProfile.get();
+
+    console.log("Hello:", myProfile.first_name);
+
+    let websites = await CrispClient.userWebsites.get();
+    console.log('Websites:', websites);
+    
+    for(let site of websites) {
+      await processSite(site);
+      await processConversations(site);
+    }
+
+    console.log('Finished.');
+    process.exit(0);
+  } catch (e) {
+    console.error('ERROR: Encountered an Error while calling the API:', e);
+  }
 }
 
 main();
